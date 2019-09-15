dc8b6e76376a9b35c44e6e24c4fa4163790acae7
Francesco Sganga <sgangacreations@gmail.com>
2019-09-15 19:55:15 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,7 +10,6 @@
 		<script type="text/javascript" src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-		<script src="https://ubilabs.github.io/geocomplete/jquery.geocomplete.js"></script>
 		<script type="text/javascript" src="{{url}}/assets/js/amiciviaggiando.js"></script>
 		<?php print $custom_js; ?>
 	</body>
